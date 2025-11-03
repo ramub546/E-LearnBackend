be430eeecf490f25ecc8d9e33187aaf3dded4da8
Shubham <shubhamsinghmys@gmail.com>
2025-11-03 13:34:23 +0530

    Seed Classes & Subjects Updated to add syllabus in the description field

--- models/Class.js
+++ models/Class.js
@@ -1,23 +1,28 @@
 const mongoose = require('mongoose');
 
-const classSchema = new mongoose.Schema({
-  className: {
-    type: String,
-    required: true,
-    unique: true,
-    enum: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+const classSchema = new mongoose.Schema(
+  {
+    className: {
+      type: String,
+      required: true,
+      unique: true,
+      enum: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    },
+    classCode: {
+      type: String,
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+    subjects: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Subject',
+      },
+    ],
   },
-  classCode: {
-    type: String,
-    unique: true,
-  },
-  description: {
-    type: String,
-  },
-  subjects: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Subject',
-  }],
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Class', classSchema);
